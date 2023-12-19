2720cb29165813bfd0e378d02531f0ba1f589a63
BrandonLazl <152586536+BrandonLazl@users.noreply.github.com>
2023-12-19 09:22:13 -0600

    programa principal update
    se añadio el menu (sin comportamiento)

--- Program.cs
+++ Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace ExamenOrdinario
 {
-    class Program
+     class Program
     {
         static void Main(string[] args)
         {
-        }
+              while (true) 
+          {
+
+  Console.WriteLine(" --------------- M E N U ---------------");
+  Console.WriteLine("|[1] -      Administracion del centro    |");
+  Console.WriteLine("|[2] -    Administracion de adopciones   |");
+  Console.WriteLine("|[3] - Administracion de bienestar animal|");
+  Console.WriteLine("|[4] -     Simulación de interacciones   |");
+  Console.WriteLine("|[5] -         Finalizar programa        |");
+  Console.WriteLine(" ----------------------------------------");
+
+  int respuesta = int.Parse(Console.ReadLine());
+
+  switch (respuesta)
+  {
+
+      case 1:
+
+          AdministracionCentro();
+
+          break;
+
+   }
+  
+  }
+
+            void AdministracionCentro(bool checkWhile = true)
+
+            {
+            
+            }
     }
 }
