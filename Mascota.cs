ccc41af2204373365503f9c8a8d6fe40ce3328b0
BrandonLazl <152586536+BrandonLazl@users.noreply.github.com>
2023-12-19 09:20:31 -0600

    Clase mascota update
    se añadieron propiedades y comportamientos a la clase mascota

--- Mascota.cs
+++ Mascota.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace ExamenOrdinario
 {
-    class Mascota
+   class Mascota
     {
+        public int id { get; set; }
+        public string nombre { get;}
+        public string temperamento { get; set; }
+        public int edad { get;}
+        public string especie { get;}
+
+        public void HacerRuido()
+        {
+            if (especie == "Perro")
+            {
+                Console.WriteLine("guau guau");
+            }
+            else if (especie == "Gato")
+            {
+                Console.WriteLine("miau miau");
+            }
+            else if (especie== "Capibara")
+            {
+                Console.WriteLine("cui cui");
+            }
+        }
+        public void CambiarDeDueño()
+        {
+
+        }
     }
 }
