178027ee379ac9004dfe760f99f01ac3d0da59b7
BrandonLazl <152586536+BrandonLazl@users.noreply.github.com>
2023-12-19 11:29:05 -0600

    Update Persona.cs
    se añadieron cambios a la clase persona

--- Persona.cs
+++ Persona.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +10,23 @@ namespace ExamenOrdinario
     {
         public int id { get; set; }
         public string nombre { get; set; }
+
+        public void ObtenerMascota()
+        {
+
+        }
+        public void ObtenerMascotaPorID()
+        {
+
+        }
+        public void AgregarMascota()
+        {
+         
+        }
+        public void Acariciar()
+        {
+
+        }
+     
     }
 }
