abfdbc7f057b1d1fdf09f2b8803dc83251e2c744
BrandonLazl <152586536+BrandonLazl@users.noreply.github.com>
2023-12-19 10:01:33 -0600

    Add files via upload

---
+++ Capibara.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenOrdinario
+{
+    class Capibara
+    {
+        public int id { get; set; }
+        public string nombre { get; }
+        public int edad { get; }
+        public string especie { get; }
+
+        public void HacerRuido()
+        {
+            Console.WriteLine("cui cui");
+        }
+        public void CambiarDeDueño()
+        {
+
+        }
+    }
+}
