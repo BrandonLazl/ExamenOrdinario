9f85e4ba8d560029e8273cbf2ea777819b22ab81
BrandonLazl <152586536+BrandonLazl@users.noreply.github.com>
2023-12-19 10:01:53 -0600

    Add files via upload

---
+++ Gato.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenOrdinario
+{
+
+    public enum temperamento
+    {
+      
+    }
+    class Gato
+    {
+        public int id { get; set; }
+        public string nombre { get; }
+        public int edad { get; }
+        public string especie { get; }
+
+        public void HacerRuido()
+        {
+            Console.WriteLine("miau miau");
+        }
+        public void CambiarDeDueño()
+        {
+
+        }
+        public void ResponderAcaricia()
+        {
+          
+        }
+    }
+}
