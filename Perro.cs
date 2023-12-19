a8cf23c22d2e9faf9c7f83c9f8c8699304afe691
BrandonLazl <152586536+BrandonLazl@users.noreply.github.com>
2023-12-19 09:35:41 -0600

    clase perro
    se añadieron comportamientos y propiedades a la clase perro

--- Perro.cs
+++ Perro.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace ExamenOrdinario
 {
-    class Perro
+    class Perro: IAcariciable
     {
+          public int id { get; set; }
+        public string nombre { get; }
+        public int edad { get; }
+        public string especie { get; }
+
+        public void HacerRuido()
+        {
+            Console.WriteLine("guau guau");
+        }
+        public void CambiarDeDueño()
+        {
+
+        }
+        public void ResponderAcaricia()
+        {
+            Console.WriteLine($"{nombre} se pone feliz y mueve la cola :)");
+        }
     }
 }
