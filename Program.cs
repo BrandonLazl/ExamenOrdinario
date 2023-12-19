6c4081c94568a9c7009ba044b6af1912a9915574
BrandonLazl <152586536+BrandonLazl@users.noreply.github.com>
2023-12-19 11:27:45 -0600

    Update Program.cs
    se añadieron los cambios de la rama development

--- Program.cs
+++ Program.cs
@@ -8,38 +8,310 @@ namespace ExamenOrdinario
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-              while (true) 
-          {
+            List<string> personas = new List<string>();
+            List<string> mascotas = new List<string>();
+         
+            while (true)
+            {
 
-  Console.WriteLine(" --------------- M E N U ---------------");
-  Console.WriteLine("|[1] -      Administracion del centro    |");
-  Console.WriteLine("|[2] -    Administracion de adopciones   |");
-  Console.WriteLine("|[3] - Administracion de bienestar animal|");
-  Console.WriteLine("|[4] -     Simulación de interacciones   |");
-  Console.WriteLine("|[5] -         Finalizar programa        |");
-  Console.WriteLine(" ----------------------------------------");
+                Console.WriteLine(" --------------- M E N U ---------------");
+                Console.WriteLine("|[1] -      Administracion del centro    |");
+                Console.WriteLine("|[2] -    Administracion de adopciones   |");
+                Console.WriteLine("|[3] - Administracion de bienestar animal|");
+                Console.WriteLine("|[4] -     Simulación de interacciones   |");
+                Console.WriteLine("|[5] -         Finalizar programa        |");
+                Console.WriteLine(" ----------------------------------------");
 
-  int respuesta = int.Parse(Console.ReadLine());
+                int respuesta = int.Parse(Console.ReadLine());
 
-  switch (respuesta)
-  {
+                 switch (respuesta)
+ {
 
-      case 1:
+         case 1:
 
-          AdministracionCentro();
+             AdministracionCentro();
+
+         break;
+
+          case 2:
+
+             AdmintracionDeAdopcion();
 
           break;
 
-   }
-  
-  }
+          case 3:
 
-            void AdministracionCentro(bool checkWhile = true)
+             AdministracionDelBienestarAnimal();
+
+          break;
+             
+         case 4:
+
+             SimulacionDeInteracciones();
+
+         break;
+
+         case 5:
+
+             Console.WriteLine("Hasta luego :D");
+
+             Environment.Exit(0);
+
+         break;
+
+         default:
+
+             Console.WriteLine("Ingrese un numero valido >:(");
+
+             break;
+
+
+  }
+ 
+ }
+
+            void AdministracionCentro(bool checkWhile = true, bool checkWhile_2 = true)
 
             {
-            
+
+                while (checkWhile)
+                {
+
+                    Console.WriteLine(" ------- A D M I N _ C E N T R O --------");
+                    Console.WriteLine("|[1] -     Administracion de personas    |");
+                    Console.WriteLine("|[2] -     Administracion de mascotas    |");
+                    Console.WriteLine("|[3] -     Regresar a menú principal     |");
+                    Console.WriteLine(" ----------------------------------------");
+
+                    int respuesta_ADMINCentro = int.Parse(Console.ReadLine());
+
+                    switch (respuesta_ADMINCentro)
+                    {
+
+                        case 1:
+
+                            while (checkWhile_2)
+                            {
+
+                                Console.WriteLine(" ----- A D M I N _ P E R S O N A S ------");
+                                Console.WriteLine("|[1] -     Mostrar todas las personas    |");
+                                Console.WriteLine("|[2] -      Registrar persona nueva      |");
+                                Console.WriteLine("|[3] -     Buscar persona por nombre     |");
+                                Console.WriteLine("|[4] -         Examinar persona          |");
+                                Console.WriteLine("|[5] -     Regresar al menu anterior     |");
+                                Console.WriteLine(" ----------------------------------------");
+
+                                int respuesta_ADMINPeronas = int.Parse(Console.ReadLine());
+
+                                switch (respuesta_ADMINPeronas)
+                                {
+
+                                    case 1:
+                                        Console.WriteLine($"{personas}");
+                                        break;
+
+                                    case 2:
+                                        Persona persona = new Persona();
+                                        personas.Add(persona.nombre);
+                                        break;
+
+                                    case 3:
+
+                                        break;
+
+                                    case 4:
+                                        break;
+
+                                    case 5:
+
+                                        checkWhile_2 = false;
+
+                                        break;
+
+                                    default:
+
+                                        Console.WriteLine("Ingrese un numero valido >:(");
+
+                                        break;
+                                }
+
+
+                            }
+
+                            break;
+
+                        case 2:
+
+                            Console.WriteLine(" ----- A D M I N _ M A S C O T A S ------");
+                            Console.WriteLine("|[1] -     Mostrar todas las mascotas    |");
+                            Console.WriteLine("|[2] -      Registrar mascota nueva      |");
+                            Console.WriteLine("|[3] -     Buscar mascota por nombre     |");
+                            Console.WriteLine("|[4] -    Buscar mascota por especie     |");
+                            Console.WriteLine("|[5] -     Regresar al menu anterior     |");
+                            Console.WriteLine(" ----------------------------------------");
+
+                            switch (respuesta_ADMINCentro)
+                            {
+
+                                case 1:
+                                    Console.WriteLine($"{mascotas}");
+                                    break;
+
+                                case 2:
+                                    Perro perro = new Perro();
+                                    Gato gato = new Gato();
+                                    Capibara capibara = new Capibara();
+
+                                    Console.WriteLine("Seleccione una especie: 1)Perro, 2)Gato, 3)Capibara");
+                                    int respuesta = int.Parse(Console.ReadLine());
+                                    if (respuesta == 1)
+                                    {
+                                        mascotas.Add(perro.nombre);
+                                    }
+                                    else if (respuesta == 2)
+                                    {
+                                        mascotas.Add(gato.nombre);
+                                    }
+                                    else if (respuesta == 3)
+                                    {
+                                        mascotas.Add(capibara.nombre);
+                                    }
+                                    break;
+
+                                case 3:
+                                    break;
+
+                                case 4:
+                                    break;
+
+                                case 5:
+
+                                    checkWhile_2 = false;
+
+                                    break;
+
+                                default:
+                                    break;
+                            }
+
+                            break;
+
+                        case 3:
+
+                            checkWhile = false;
+
+                            break;
+
+                        default:
+
+                            Console.WriteLine("Ingrese un numero valido >:(");
+
+                            break;
+
+                    }
+
+                }
+
+
             }
+
+                        void AdmintracionDeAdopcion(bool checkWhile = true)
+            {
+                while (checkWhile) 
+                {
+                
+                Console.WriteLine(" --- A D M I N _ A D O P C I O N E S ----");
+                Console.WriteLine("|[1] - Mascotas disponibles por adoptar  |");
+                Console.WriteLine("|[2] -          Adoptar mascota          |");
+                Console.WriteLine("|[3] -    Regresar a la menú anterior    |");
+                Console.WriteLine(" ----------------------------------------");
+
+                int respuesta = int.Parse(Console.ReadLine());
+
+                switch (respuesta)
+                {
+
+                    case 1:
+                        break;
+
+                    case 2:
+                        break;
+
+                    case 3:
+
+                            checkWhile = false;
+
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Ingrese un numero valido >:(");
+
+                        break;
+                    }
+                
+                
+                }
+                
+
+            }
+
+            void AdministracionDelBienestarAnimal(bool checkWhile = true) 
+            {
+
+                while (checkWhile)
+                {
+
+                    Console.WriteLine(" --------------- M E N U ---------------");
+                    Console.WriteLine("|[1] -       Servicio  de Spa           |");
+                    Console.WriteLine("|[2] -         Corte de pelo            |");
+                    Console.WriteLine("|[3] -    Volver al menu anterior       |");
+                    Console.WriteLine(" ----------------------------------------");
+
+                    int respuesta = int.Parse(Console.ReadLine());
+
+                    switch (respuesta)
+                    {
+
+                        case 1:
+
+
+                            break;
+
+                        case 2:
+
+                            break;
+
+                        case 3:
+
+                            checkWhile = false;
+
+                            break;
+
+                        default:
+
+                            Console.WriteLine("Ingrese un numero valido >:(");
+
+                            break;
+                    }
+
+                }
+
+            }
+
+            void SimulacionDeInteracciones()
+            {
+
+                Console.WriteLine("No hubo tiempo :/");
+
+            }
+
+        }        
+
     }
+
 }
